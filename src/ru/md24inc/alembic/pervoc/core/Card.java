e42a7c76b37e56de3717ea2a322c60747619a3d5
Alexandr Panov <i.miroque@gmail.com>
2012-06-01 20:58:37 +0800

    Добавил методов в класс Карточки. Без конструктора этот класс на сейчас.

--- src/ru/md24inc/alembic/pervoc/core/Card.java
+++ src/ru/md24inc/alembic/pervoc/core/Card.java
@@ -1,5 +1,29 @@
 package src.ru.md24inc.alembic.pervoc.core;
 
 public class Card {
-
+	// Не знаю особо зачем эта переменная, но для эксперимента обучения и для будущего.
+	// возможно потом удалю или еще чего, пока не понял.
+	//public static int cardId;
+	private Word word;
+	private Transcript transcript;
+	private Translation translation;
+	public String getWord() {
+		return word.getWord();
+	}
+	public void setWord(String word) {
+		this.word.setWord(word);
+	}
+	public String getTranscript() {
+		return transcript.getTranscript();
+	}
+	public void setTranscript(String transcript) {
+		this.transcript.setTranscript(transcript);
+	}
+	public String getTranslation() {
+		return translation.getTranslation();
+	}
+	public void setTranslation(String translation) {
+		this.translation.setTranslation(translation);
+	}
+	
 }
