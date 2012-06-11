21819baf5b74276e821cd86ef4ef0bf13453bfea
miroque <i.miroque@gmail.com>
2012-06-11 22:45:05 +0800

    Добавил коментариев под ява документацию

--- src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
+++ src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
@@ -2,8 +2,16 @@ package src.ru.md24inc.alembic.pervoc.core;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class Vocabulary for making XML to object
+ *
+ * @author Alexander Panov
+ *
+ * @version 0.1
+ */
+
 public class Vocabulary {
-	
+
 	protected List <Card> voc = new ArrayList<Card>();
 	
 	public void add(Card c){
