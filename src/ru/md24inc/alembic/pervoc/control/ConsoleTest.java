96227ead239bdf5e7420ab75f47b0648806c0e3e
Alexandr Panov <i.miroque@gmail.com>
2012-06-19 08:57:49 +0800

    Added more Cards for "longer" printing.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleTest.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleTest.java
@@ -8,16 +8,22 @@ public class ConsoleTest {
 		Vocabulary myVocabulary = new Vocabulary();
 
 		// Filling each card with some words and info
-		Card[] myCards = new Card[3];
+		Card[] myCards = new Card[6];
 		// добавил спец символов утф-8
 		myCards[0] = new Card("Test", "S\u00ED Se\u00F1or", "Кракозябра");
 		myCards[1] = new Card("God", "gad", "Бог");
 		myCards[2] = new Card("hub", "Hab", "Концентратор. или место сборки");
+		myCards[3] = new Card("Test", "S\u00ED Se\u00F1or", "Кракозябра");
+		myCards[4] = new Card("God", "gad", "Бог");
+		myCards[5] = new Card("hub", "Hab", "Концентратор. или место сборки");
 
 		// Inserting cards into vocabulary
 		myVocabulary.add(myCards[0]);
 		myVocabulary.add(myCards[1]);
 		myVocabulary.add(myCards[2]);
+		myVocabulary.add(myCards[3]);
+		myVocabulary.add(myCards[4]);
+		myVocabulary.add(myCards[5]);
 
 		// Printing Vocabular
 		myVocabulary.printAll();
