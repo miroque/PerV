e4c0a50fd1405bf9852aa626b84680ea734208a9
Alexandr Panov <i.miroque@gmail.com>
2012-06-19 19:33:22 +0800

    Белиберда всякая по поводу, предложения пользователю о вводе имени файла в случае ошибки, повторить еще.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleFileXML.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleFileXML.java
@@ -4,7 +4,7 @@ import src.ru.md24inc.alembic.pervoc.core.*;
 import java.io.*;
 
 public class ConsoleFileXML {
-	public static void main(String args[]) throws IOException {
+	public static String promtToFile() {
 		// Creating Console promt for filename
 		Console c = System.console();
 		if (c == null) {
@@ -12,26 +12,32 @@ public class ConsoleFileXML {
 			System.exit(1);
 		}
 		String fileName = c.readLine("Enter your fileName: ");
+		return fileName;
+	}
+
+	public static void main(String args[]) throws IOException {
 		// Open file
 		BufferedReader buff = null;
-		try {
-			FileReader file = new FileReader(fileName);
-			buff = new BufferedReader(file);
-			boolean eof = false;
-			while (!eof) {
-				String line = buff.readLine();
-				if (line == null)
-					eof = true;
-				else
-					// Printing file to console
-					System.out.println(line);
+		for (int i = 0; i < 3; i++) {
+			try {
+				FileReader file = new FileReader(promtToFile());
+				buff = new BufferedReader(file);
+				boolean eof = false;
+				while (!eof) {
+					String line = buff.readLine();
+					if (line == null)
+						eof = true;
+					else
+						// Printing file to console
+						System.out.println(line);
+				}
+			} catch (IOException e) {
+				i=3;
+				System.out.println("Error -- " + e.getMessage());
+			} finally {
+				buff.close();
 			}
-		} catch (IOException e) {
-			System.out.println("Error -- " + e.toString());
-		} finally {
-			buff.close();
 		}
-
 		// Printing file to console
 
 	}
