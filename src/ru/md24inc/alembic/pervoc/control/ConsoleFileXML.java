ecda4bc6c095359517efc173b2c23e7f0bb8eed4
Alexandr Panov <i.miroque@gmail.com>
2012-06-19 14:51:50 +0800

    Играюсь по поводу чтения файла и вывода его в консоль. Дальше буду играться по поводу приглашения в строке консоли напечатать имя файла для печати в консоль.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleFileXML.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleFileXML.java
@@ -1,5 +1,31 @@
 package src.ru.md24inc.alembic.pervoc.control;
 
-public class ConsoleFileXML {
+import src.ru.md24inc.alembic.pervoc.core.*;
+import java.io.*;
 
+public class ConsoleFileXML {
+	public static void main(String args[]) throws IOException {
+		// Open file
+		BufferedReader buff = null;
+		try {
+			FileReader file = new FileReader("d:/workspace/PerV/Xclay/mud - копия.xml");
+			buff = new BufferedReader(file);
+			boolean eof = false;
+			while (!eof) {
+				String line = buff.readLine();
+				if (line == null)
+					eof = true;
+				else
+					// Printing file to console
+					System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println("Error -- " + e.toString());
+		} finally {
+			buff.close();
+		}
+
+		// Printing file to console
+
+	}
 }
