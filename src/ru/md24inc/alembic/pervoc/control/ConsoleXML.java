cad6d0b7cec08456764a7daaf77b7d86132902e6
Alexandr Panov <i.miroque@gmail.com>
2012-06-24 21:49:22 +0800

    Exploring parsing of XML with SAX. Only root element for now. But it's interesting experience.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXML.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXML.java
@@ -1,16 +1,29 @@
 package src.ru.md24inc.alembic.pervoc.control;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+
+import org.jdom2.*;
+import org.jdom2.input.*;
 
 import src.ru.md24inc.alembic.pervoc.core.*;
 
 public class ConsoleXML {
-	public final static String file = "D:\\workspace\\PerV\\Xclay\\mud.xml";
+	public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws JDOMException, IOException{
 		// Open XML File
+		SAXBuilder sax = new SAXBuilder();
+		File file = new File(srcOfFile);
+		Document doc = sax.build(file);
+		// Parcing XML Tree, getting Root Element and it's attributes
+		Element root = doc.getRootElement();
+		List <Attribute> rootAttrs = root.getAttributes();
+		System.out.println("Root element: " + root.getName());
+		for (Attribute at : rootAttrs) System.out.println("attr: " + at.getName() + " = " + at.getValue());
 		
 		// Parcing XML Tree to Java Objects
 		// Printing to System.out with tub
