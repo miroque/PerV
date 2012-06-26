d5e4533d0efdc7b2d073b7edc102566833a19a3b
Alexandr Panov <i.miroque@gmail.com>
2012-06-26 21:56:19 +0800

    Working parse XML to Java Objects. Printing to console.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
@@ -6,8 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.jdom2.*;
 import org.jdom2.input.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import src.ru.md24inc.alembic.pervoc.core.*;
 
@@ -16,35 +23,56 @@ public class ConsoleXMLObj {
 	public Vocabulary myVoc = new Vocabulary();
 
 	public static void main(String args[]) throws JDOMException, IOException {
-		// Open XML File
-		SAXBuilder sax = new SAXBuilder();
-		File file = new File(srcOfFile);
-		Document doc = sax.build(file);
+		// instantiate Vocabulary object
+		Vocabulary vocaBul = new Vocabulary();
+		Card card;
 
-		// Parsing XML Tree, getting Root Element
-		Element root = doc.getRootElement();
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(srcOfFile);
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("card");
+			System.out.println(nList.getLength());
 
-		// Parsing XML Tree, getting Children Element and it's attributes
-		List<Element> elements = root.getChildren();
-		for (Element element : elements) {
-			parseChild(element);// Our method for parsing child elements
+			for (int i = 0; i < nList.getLength(); i++) {
+				card = new Card();
+
+				Node nNode = nList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					card.setWord(getTagValue(("word"), eElement));
+					card.setTranscript(getTagValue(("transcript"), eElement));
+					card.setTranslation(getTagValue(("translation"), eElement));
+				}
+
+				// add a person to an address book
+				vocaBul.add(card);
+
+			}// end for
+
 		}
 
-		// Parsing XML Tree to Java Objects
-		// Printing to System.out with tub
-	}
-
-	private static void parseChild(Element element) {
-		List<Element> children = element.getChildren();
-		for (Element child : children) {
-			child.getValue();
-
-			parseChild(child);
+		catch (Exception e) {
+			System.out.println(e);
 		}
+		
+		vocaBul.printAll();
 
 	}
 
 	public static void openXMLFile(String fileName) {
 		// Opens xml file, sort of.
+	}
+
+	private static String getTagValue(String sTag, Element eElement) {
+		NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
+				.getChildNodes();
+
+		Node nValue = (Node) nlList.item(0);
+
+		return nValue.getNodeValue();
 	}
 }
