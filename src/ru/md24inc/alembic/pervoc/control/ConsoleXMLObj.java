466f8e7c6e1b7407ff2336cb00350993c44f5930
Alexandr Panov <i.miroque@gmail.com>
2012-06-27 12:26:36 +0800

    Clear and sweet

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
@@ -1,27 +1,11 @@
 package src.ru.md24inc.alembic.pervoc.control;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.jdom2.*;
-import org.jdom2.input.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import src.ru.md24inc.alembic.pervoc.core.*;
 
 public class ConsoleXMLObj {
 	public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
 
-	public static void main(String args[]) throws JDOMException, IOException {
+	public static void main(String args[]){
 		// How i would like to see this process?
 		Vocabulary vocaBul = new Vocabulary();
 		vocaBul.openXMLFile(srcOfFile);
