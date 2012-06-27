9b76c5a013545b4da70af3bde23c59b34e27d3d1
Alexandr Panov <i.miroque@gmail.com>
2012-06-27 12:09:05 +0800

    Draft - Added more sophisticated method for open XML file.

--- src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
+++ src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
@@ -3,6 +3,14 @@ package src.ru.md24inc.alembic.pervoc.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**
  * Class Vocabulary for making XML to object
  * 
@@ -36,5 +44,58 @@ public class Vocabulary {
 		}
 
 	}
+	// instantiate Vocabulary object
+	Vocabulary vocaBul = new Vocabulary();
+	Card card;
+
+	try {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+				.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(srcOfFile);
+		doc.getDocumentElement().normalize();
+		NodeList nList = doc.getElementsByTagName("card");
+		System.out.println(nList.getLength());
+
+		for (int i = 0; i < nList.getLength(); i++) {
+			card = new Card();
+
+			Node nNode = nList.item(i);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) nNode;
+				card.setWord(getTagValue(("word"), eElement));
+				card.setTranscript(getTagValue(("transcript"), eElement));
+				card.setTranslation(getTagValue(("translation"), eElement));
+			}
+
+			// add a person to an address book
+			vocaBul.add(card);
+
+		}// end for
+
+	}
+
+	catch (Exception e) {
+		System.out.println(e);
+	}
+	
+	vocaBul.printAll();
+
+}
+
+public static void openXMLFile(String fileName) {
+	// Opens xml file, sort of.
+}
+
+private static String getTagValue(String sTag, Element eElement) {
+	NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
+			.getChildNodes();
+
+	Node nValue = (Node) nlList.item(0);
+
+	return nValue.getNodeValue();
+}
+
 
 }
