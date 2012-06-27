7466fce8a289d1af9ee1acd030824e3ea021fddf
Alexandr Panov <i.miroque@gmail.com>
2012-06-27 15:04:35 +0800

    Errased waste comment

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
@@ -6,7 +6,6 @@ public class ConsoleXMLObj {
 	public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
 
 	public static void main(String args[]){
-		// How i would like to see this process?
 		Vocabulary vocaBul = new Vocabulary();
 		vocaBul.openXMLFile(srcOfFile);
 		vocaBul.printVocabular();
