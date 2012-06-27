6ca0e8c11e94292862b7b4b4ead0ba6ac8f94ca7
Alexandr Panov <i.miroque@gmail.com>
2012-06-27 12:11:53 +0800

    Clear waste code

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
@@ -20,59 +20,11 @@ import src.ru.md24inc.alembic.pervoc.core.*;
 
 public class ConsoleXMLObj {
 	public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
-	public Vocabulary myVoc = new Vocabulary();
 
 	public static void main(String args[]) throws JDOMException, IOException {
-		// instantiate Vocabulary object
+		// How i would like to see this process?
 		Vocabulary vocaBul = new Vocabulary();
-		Card card;
-
-		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(srcOfFile);
-			doc.getDocumentElement().normalize();
-			NodeList nList = doc.getElementsByTagName("card");
-			System.out.println(nList.getLength());
-
-			for (int i = 0; i < nList.getLength(); i++) {
-				card = new Card();
-
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-					Element eElement = (Element) nNode;
-					card.setWord(getTagValue(("word"), eElement));
-					card.setTranscript(getTagValue(("transcript"), eElement));
-					card.setTranslation(getTagValue(("translation"), eElement));
-				}
-
-				// add a person to an address book
-				vocaBul.add(card);
-
-			}// end for
-
-		}
-
-		catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		vocaBul.printAll();
-
-	}
-
-	public static void openXMLFile(String fileName) {
-		// Opens xml file, sort of.
-	}
-
-	private static String getTagValue(String sTag, Element eElement) {
-		NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
-				.getChildNodes();
-
-		Node nValue = (Node) nlList.item(0);
-
-		return nValue.getNodeValue();
+		vocaBul.openXMLFile(srcOfFile);
+		vocaBul.printVocabular();
 	}
 }
