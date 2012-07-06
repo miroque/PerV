74ba2a1251c7a31bfddd86418a292a49bee1a3af
Alexandr Panov <i.miroque@gmail.com>
2012-07-06 21:34:03 +0800

    Дописал метод Сохранения файла. правда пока там только один рутовский элемент сохранятеся.

--- src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
+++ src/ru/md24inc/alembic/pervoc/core/Vocabulary.java
@@ -1,10 +1,17 @@
 package ru.md24inc.alembic.pervoc.core;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,113 +20,135 @@ import org.w3c.dom.NodeList;
 
 /**
  * Class Vocabulary for making XML to object
- * 
+ *
  * @author Alexander Panov
- * 
+ *
  * @version 0.2
  */
-
 public class Vocabulary {
 
-	protected List<Card> voc;
+    protected List<Card> voc;
 
-	/**
-	 * Class constructor.
-	 */
+    /**
+     * Class constructor.
+     */
+    public Vocabulary() {
+        System.out.println("Initial Vocab");
+        voc = new ArrayList<Card>();
+    }
 
-	public Vocabulary() {
-		System.out.println("Initial Vocab");
-		voc = new ArrayList<Card>();
-	}
-	
-	/**
-	 * Adding a Word Card to list of Cards
-	 * @param card 
-	 */
+    /**
+     * Adding a Word Card to list of Cards
+     *
+     * @param card
+     */
+    public void addCard(Card card) {
+        voc.add(card);
+    }
 
-	public void addCard(Card card) {
-		voc.add(card);
-	}
-	
-	/**
-	 * Printing to console the content of vocabulary
-	 * in tabs columns
-	 */
+    /**
+     * Printing to console the content of vocabulary in tabs columns
+     */
+    public void printVocabular() {
+        System.out.println("Printing All Cards");
+        for (int i = 0; i < voc.size(); i++) {
+            System.out.println("\t" + voc.get(i).getWord() + " \t["
+                    + voc.get(i).getTranscript() + "] \t"
+                    + voc.get(i).getTranslation());
+        }
 
-	public void printVocabular() {
-		System.out.println("Printing All Cards");
-		for (int i = 0; i < voc.size(); i++) {
-			System.out.println("\t" + voc.get(i).getWord() + " \t["
-					+ voc.get(i).getTranscript() + "] \t"
-					+ voc.get(i).getTranslation());
-		}
+    }
 
-	}
-	
-	/**
-	 * Opens XML file of Vocabulary and fills up Vocabulary
-	 * @param fileName String for source XML file
-	 */
-	//TODO Add some kind of Error Exceptions
+    /**
+     * Opens XML file of Vocabulary and fills up Vocabulary
+     *
+     * @param fileName String for source XML file
+     */
+    //TODO Add some kind of Error Exceptions
+    public void openXMLFile(String fileName) {
+        Card card;
 
-	public void openXMLFile(String fileName) {
-		Card card;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fileName);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("card");
 
-		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fileName);
-			doc.getDocumentElement().normalize();
-			NodeList nList = doc.getElementsByTagName("card");
+            for (int i = 0; i < nList.getLength(); i++) {
+                card = new Card();
 
-			for (int i = 0; i < nList.getLength(); i++) {
-				card = new Card();
+                Node nNode = nList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    card.setWord(getTagValue(("word"), eElement));
+                    card.setTranscript(getTagValue(("transcript"), eElement));
+                    card.setTranslation(getTagValue(("translation"), eElement));
+                }
 
-					Element eElement = (Element) nNode;
-					card.setWord(getTagValue(("word"), eElement));
-					card.setTranscript(getTagValue(("transcript"), eElement));
-					card.setTranslation(getTagValue(("translation"), eElement));
-				}
+                voc.add(card);
 
-				voc.add(card);
+            }// end for
 
-			}// end for
+        }// end try
+        catch (Exception e) {
+            System.out.println(e);
+        }
 
-		}// end try
+    }
 
-		catch (Exception e) {
-			System.out.println(e);
-		}
+    /**
+     * Saves Vocabulary to XML file
+     *
+     * @param fileName String for output XML file
+     */
+    //TODO Add some kind of Error Exceptions
+    public void saveXMLFile(String fileName) {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-	}
-	
-	/**
-	 * Saves Vocabulary to XML file
-	 * @param fileName String for output XML file
-	 */
-	//TODO Add some kind of Error Exceptions
-	
-	public void saveXMLFile(String fileName) {
-	}
+            //root element
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("vocabulary");
+            doc.appendChild(rootElement);
 
-	/**
-	 * Inner Method for getting Value of Node Chiled by it's Name
-	 * @param sTag
-	 * @param eElement
-	 * @return the Value of Node
-	 */
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(fileName));
 
-	private static String getTagValue(String sTag, Element eElement) {
-		NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
-				.getChildNodes();
+            // Output to console for testing
+            // StreamResult result = new StreamResult(System.out);
 
-		Node nValue = (Node) nlList.item(0);
+            transformer.transform(source, result);
 
-		return nValue.getNodeValue();
-	}
+            System.out.println("File saved!");
 
+
+
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
+    }
+
+    /**
+     * Inner Method for getting Value of Node Chiled by it's Name
+     *
+     * @param sTag
+     * @param eElement
+     * @return the Value of Node
+     */
+    private static String getTagValue(String sTag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+
+        Node nValue = (Node) nlList.item(0);
+
+        return nValue.getNodeValue();
+    }
 }
