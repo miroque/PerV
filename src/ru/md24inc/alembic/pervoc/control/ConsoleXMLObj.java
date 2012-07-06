685cb88f57a960b2b68b60a293acfa437dc860d6
Alexandr Panov <i.miroque@gmail.com>
2012-07-06 21:34:34 +0800

    Добавил для проверки метод сохранения.

--- src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
+++ src/ru/md24inc/alembic/pervoc/control/ConsoleXMLObj.java
@@ -3,11 +3,14 @@ package ru.md24inc.alembic.pervoc.control;
 import ru.md24inc.alembic.pervoc.core.*;
 
 public class ConsoleXMLObj {
-	public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
+    //public final static String srcOfFile = "D:\\workspace\\PerV\\Xclay\\mud.xml";
 
-	public static void main(String args[]){
-		Vocabulary vocaBul = new Vocabulary();
-		vocaBul.openXMLFile(srcOfFile);
-		vocaBul.printVocabular();
-	}
+    public final static String srcOfFile = "c:\\Users\\miroque\\Documents\\NetBeansProjects\\PerV\\Xclay\\mud.xml";
+
+    public static void main(String args[]) {
+        Vocabulary vocaBul = new Vocabulary();
+        vocaBul.openXMLFile(srcOfFile);
+        vocaBul.printVocabular();
+        vocaBul.saveXMLFile("c:\\Users\\miroque\\Documents\\NetBeansProjects\\PerV\\Xclay\\mudSaved.xml");
+    }
 }
