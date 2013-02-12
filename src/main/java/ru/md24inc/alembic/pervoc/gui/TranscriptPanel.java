d93d576f765d61d53240cd0951082bbde83f41b9
miroque <i.miroque@gmail.com>
2013-02-12 13:24:09 +0800

    Some play with TranscriptPanel

--- src/main/java/ru/md24inc/alembic/pervoc/gui/TranscriptPanel.java
+++ src/main/java/ru/md24inc/alembic/pervoc/gui/TranscriptPanel.java
@@ -1,6 +1,10 @@
 package ru.md24inc.alembic.pervoc.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import javax.swing.*;
 
@@ -10,9 +14,14 @@ public class TranscriptPanel extends JPanel {
 	
 	public TranscriptPanel (){
 		for (int i=0; i<10; i++){
-			symbols[i]=new JTextField("i"+i);
+			symbols[i]=new JTextField();
+			symbols[i].setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
 			add(symbols[i]);
 		}
+		symbols[0].setText("\u0243");
+		symbols[1].setText("\u0246");
+		symbols[2].setText("\u02A4");
+		symbols[3].setText("\u02A7");
 		
 	}
 	
