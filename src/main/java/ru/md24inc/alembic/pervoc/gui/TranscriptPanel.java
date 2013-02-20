d81dc2f9c3b32d526cc31b02bb2b0b808568086f
Alexandr Panov <i.miroque@gmail.com>
2013-02-20 21:45:04 +0800

    some bit of optimizations

--- src/main/java/ru/md24inc/alembic/pervoc/gui/TranscriptPanel.java
+++ src/main/java/ru/md24inc/alembic/pervoc/gui/TranscriptPanel.java
@@ -16,125 +16,114 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
 
 public class TranscriptPanel extends JPanel {
-	private static final long serialVersionUID = -7784099290733841692L;
+    private static final long serialVersionUID = -7784099290733841692L;
 
-	// JTextField[] symbols;
-	JLabel[] symbols;
-	List<Object> consonants;
-	List<Object> vowels;
-	List<Object> special;
-	Border border;
+    // JTextField[] symbols;
+    JLabel[] symbols;
+    List<Object> consonants;
+    List<Object> vowels;
+    List<Object> special;
+    Border border;
 
-	public TranscriptPanel() {
-		initSymbols();
-	}
+    public TranscriptPanel() {
+        initSymbols();
+    }
 
-	private void initSymbols() {
-		// Setting up Panel properties
-		// setLayout(new GridLayout(0, 10, 2, 2));
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
-		border = BorderFactory.createLineBorder(Color.lightGray, 1);
-		// Reading xml file with phonetic symbols
-		XMLConfiguration mxconf = null;
-		try {
-			mxconf = new XMLConfiguration(new File("src/main/resources/symbols.xml"));
-		} catch (ConfigurationException e) {
-			System.out.println("Reading config failed due: " + e.getMessage());
-		}
-		// Fill up collection variables with needed symbols
-		consonants = mxconf.getList("Consonants.symbol");
-		vowels = mxconf.getList("Vowels.symbol");
-		special = mxconf.getList("Special.symbol");
+    private void initSymbols() {
+        // Setting up Panel properties
+        // setLayout(new GridLayout(0, 10, 2, 2));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        border = BorderFactory.createLineBorder(Color.lightGray, 1);
+        // Reading xml file with phonetic symbols
+        XMLConfiguration mxconf = null;
+        try {
+            mxconf = new XMLConfiguration(new File("src/main/resources/symbols.xml"));
+        } catch (ConfigurationException e) {
+            System.out.println("Reading config failed due: " + e.getMessage());
+        }
+        // Fill up collection variables with needed symbols
+        consonants = mxconf.getList("Consonants.symbol");
+        vowels = mxconf.getList("Vowels.symbol");
+        special = mxconf.getList("Special.symbol");
 
-		SymbolRow[] test = new SymbolRow[3];
-		test[0] = new SymbolRow();
-		test[0].setInfo("Consonants", consonants, new Color(0, 0, 150));
-		add(test[0]);
-		add(Box.createRigidArea(new Dimension(0, 10)));
-		test[1] = new SymbolRow();
-		test[1].setInfo("Vowels", vowels, new Color(150, 0, 0));
-		add(test[1]);
-		add(Box.createRigidArea(new Dimension(0, 10)));
-		test[2] = new SymbolRow();
-		test[2].setInfo("Special", special, new Color(150, 150, 0));
-		add(test[2]);
-	}
 
-	class SymbolRow extends JPanel implements MouseListener{
-		JLabel header;
-		JLabel[] symbolsHolder;
-		Border fronties = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1);
-		GridLayout layoutForSymbols;
-		BorderLayout layoutForAll;
-		JPanel headerPanel;
-		JPanel symbolsPanel;
+        add(new SymbolRow("Consonants", consonants, new Color(0, 0, 150)));
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(new SymbolRow("Vowels", vowels, new Color(150, 0, 0)));
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(new SymbolRow("Special", special, new Color(150, 150, 0)));
+    }
 
-		public SymbolRow() {
-			layoutForSymbols = new GridLayout(0, 10, 2, 2);
-			layoutForAll = new BorderLayout(5, 5);
-			headerPanel = new JPanel();
-			symbolsPanel = new JPanel(layoutForSymbols);
-			header = new JLabel();
-			setLayout(layoutForAll);
-			add(headerPanel, BorderLayout.WEST);
-			add(symbolsPanel, BorderLayout.CENTER);
-		}
+    class SymbolRow extends JPanel implements MouseListener {
+        JLabel header;
+        JLabel[] symbolsHolder;
+        Border fronties = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1);
+        GridLayout layoutForSymbols;
+        BorderLayout layoutForAll;
+        JPanel headerPanel;
+        JPanel symbolsPanel;
 
-		public void setInfo(String h, List<Object> in, Color color) {
-			header.setText(h);
-			headerPanel.add(header);
-			symbolsHolder = new JLabel[in.size()];
-			ListIterator<Object> it = in.listIterator();
-			int go = 0;
-			while (it.hasNext()) {
-				Object ob = it.next();
-				symbolsHolder[go] = new JLabel(ob.toString());
-				symbolsHolder[go].setBorder(fronties);
-				symbolsHolder[go].setForeground(color);
-				symbolsHolder[go].addMouseListener(this);
-				symbolsHolder[go].setHorizontalAlignment(JLabel.CENTER);
-				symbolsHolder[go].setFont(new Font(Font.SANS_SERIF, Font.PLAIN,
-						16));
-				symbolsPanel.add(symbolsHolder[go]);
-			}
-		}
+        public SymbolRow(String h, List<Object> in, Color color) {
+            layoutForSymbols = new GridLayout(0, 10, 2, 2);
+            layoutForAll = new BorderLayout(5, 5);
+            headerPanel = new JPanel();
+            symbolsPanel = new JPanel(layoutForSymbols);
+            header = new JLabel();
+            setLayout(layoutForAll);
+            add(headerPanel, BorderLayout.WEST);
+            add(symbolsPanel, BorderLayout.CENTER);
+            header.setText(h);
+            headerPanel.add(header);
+            symbolsHolder = new JLabel[in.size()];
+            int go = 0;
+            for (Object ob : in) {
+                symbolsHolder[go] = new JLabel(ob.toString());
+                symbolsHolder[go].setBorder(fronties);
+                symbolsHolder[go].setForeground(color);
+                symbolsHolder[go].addMouseListener(this);
+                symbolsHolder[go].setHorizontalAlignment(JLabel.CENTER);
+                symbolsHolder[go].setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+                symbolsPanel.add(symbolsHolder[go]);
+                go++;
+            }
+        }
 
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			JLabel ex = (JLabel) e.getComponent();
-			System.out.print(ex.getText()+" ");
-		}
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            JLabel ex = (JLabel) e.getComponent();
+            System.out.print(ex.getText() + " ");
+        }
 
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
 
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+        }
 
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
 
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+        }
 
-		
-	}
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
 
-	public void paintComponent(Graphics g) {
+        }
 
-	}
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
 
 }
